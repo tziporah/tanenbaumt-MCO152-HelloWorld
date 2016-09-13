610ae6cf7bf3b4881d87cfebc54d963609ac40ca
Tziporah Tanenbaum <ttanenba@student.touro.edu>
2016-09-13 19:11:37 -0400

    Elul message

--- src/Program.java
+++ src/Program.java
@@ -3,6 +3,7 @@ public class Program {
 	public static void main (String [] args){
 		//this line displays a message
 		System.out.println("Hello World!");
+		System.out.println("Chamol al maasecha");
 	}
 
 }
